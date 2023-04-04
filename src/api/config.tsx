d530c2d7c079bcc5d2cb11080fa3a335fecb2b37
Sebastian Carrillo <carrillosebastian1@gmail.com>
2023-04-04 08:03:16 -0500

    Chore Added environment variable to backend production

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const baseURL = 'http://192.168.100.19:8000'
+const prod = import.meta.env.VITE_BACKEND_PROD
 
 export const apiHospitals = axios.create({ 
-  baseURL
+  baseURL: prod
 })
